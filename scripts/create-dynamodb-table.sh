f219b9f630bd286a004256cd7981702b57c9460e
sang-w0o <robbyra@gmail.com>
2022-01-15 02:24:00 +0900

    feat: Add GSI to scripts

--- scripts/create-dynamodb-table.sh
+++ scripts/create-dynamodb-table.sh
@@ -6,8 +6,12 @@ aws dynamodb create-table \
   --table-name grpc-gateway-example-user \
   --attribute-definitions \
   AttributeName=user_id,AttributeType=S \
+  AttributeName=nickname,AttributeType=S \
+  AttributeName=created_at,AttributeType=S \
   --key-schema \
   AttributeName=user_id,KeyType=HASH \
+  AttributeName=nickname,KeyType=RANGE \
   --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 \
+  --global-secondary-indexes file://scripts/gsi.json \
   --endpoint-url http://localhost:54000 \
   --region ap-northeast-2 || true | cat
